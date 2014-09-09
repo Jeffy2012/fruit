3f7e11ea23b68edacd8a546ab435e4210ebd7e86
Jeffy <yuanjiefeng@gmail.com>
2014-09-09 18:05:52 +0800

    db config

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'adminuhDwPqV',
+    'password' => '6qLeK7ydVPfP',
     'charset' => 'utf8',
 ];
