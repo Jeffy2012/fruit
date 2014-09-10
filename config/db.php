29f86d377ddfc38bdaf47d5908e1bc22c2895ea4
Jeffy <yuanjiefeng@gmail.com>
2014-09-10 17:50:31 +0800

    config db

--- config/db.php
+++ config/db.php
@@ -1,11 +1,20 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    //'username' => 'adminuhDwPqV',
-    'username' => 'root',
-    //'password' => '6qLeK7ydVPfP',
-    'password' => '',
-    'charset' => 'utf8',
-];
+if(YII_ENV_DEV){
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+    ];
+}else{
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+        'username' => 'adminuhDwPqV',
+        'password' => '6qLeK7ydVPfP',
+        'charset' => 'utf8',
+    ];
+}
+
