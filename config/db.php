75b3c8209605acc27ca5440ab47c91bcb54fa019
Jeffy <yuanjiefeng@gmail.com>
2014-09-22 15:21:47 +0800

    yii2 update

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'adminuhDwPqV',
-    'password' => '6qLeK7ydVPfP',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'MqU999k5vzfFlH8ljPxi3PnBAk2A1zBH',
+            'cookieValidationKey' => 'Tx-jyPZjBneX1sVQShHg69R1nhoad5OH',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -38,10 +38,6 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => true,
-        ],
     ],
     'params' => $params,
 ];
