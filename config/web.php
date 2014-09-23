ebf6c46f553783d7ede09eac57a1bf4ff0f08c3b
Jeffy <yuanjiefeng@gmail.com>
2014-09-23 09:47:34 +0800

    yii2 update

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'Tx-jyPZjBneX1sVQShHg69R1nhoad5OH',
+            'cookieValidationKey' => 'QqMcSOuCBP0-fLZ_1_0fH8LqyxwUYXht',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
